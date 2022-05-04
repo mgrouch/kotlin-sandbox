518b59c08e11ff9f9912ed35ceb83da9bace4a6f
mgrouch <mgrushinskiy@gmail.com>
2022-05-04 09:19:17 -0400

    kotlin springboot

---
+++ kotlin-springboot/src/test/kotlin/com/example/kotlin/springboot/KotlinSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.springboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
