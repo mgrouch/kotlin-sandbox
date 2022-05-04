d5176b7590200a6f84e2a053253ea2f2bed88824
mgrouch <mgrushinskiy@gmail.com>
2022-05-04 09:29:12 -0400

    kotlin spring web

---
+++ kotlin-spring-web/src/test/kotlin/com/example/kotlin/spring/web/KotlinSpringWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.spring.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
