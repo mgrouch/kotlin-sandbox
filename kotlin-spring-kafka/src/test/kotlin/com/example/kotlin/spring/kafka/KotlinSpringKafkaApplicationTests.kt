3475d76a98c9708d5d7941d70bea2f194ff6e251
mgrouch <mgrushinskiy@gmail.com>
2022-05-04 09:29:39 -0400

    kotlin spring kafka

---
+++ kotlin-spring-kafka/src/test/kotlin/com/example/kotlin/spring/kafka/KotlinSpringKafkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.spring.kafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringKafkaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
