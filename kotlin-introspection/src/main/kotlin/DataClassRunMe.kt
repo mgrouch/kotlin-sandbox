83320cf9b033ebec2c3b49f560b84a89a2f0f30c
mgrouch <mgrushinskiy@gmail.com>
2022-05-27 11:29:29 -0400

    kotlin introspection

--- kotlin-introspection/src/main/kotlin/DataClassRunMe.kt
+++ kotlin-introspection/src/main/kotlin/DataClassRunMe.kt
@@ -3,13 +3,13 @@ fun main(args: Array<String>) {
     val obj = DataClass(1L, "name", "readOnly", -1)
     println("$obj")
 
-    // it's kind of inefficient
+    // it's kind of inefficient (if prop name is not a variable)
     println("${obj[DataClass::readOnly.name]} ${obj[DataClass::nonNull.name]}")
 
     // better just
     println("${DataClass::readOnly.get(obj)} ${DataClass::nonNull.get(obj)}")
 
-    // it's kind of inefficient
+    // it's kind of inefficient (if prop name is not a variable)
     obj[DataClass::data.name] = "name2"
     obj[DataClass::nonNull.name] = -3
     println("$obj")
