c8270cd95ddb069bac0fc05f30f0433b6d412960
mgrouch <mgrushinskiy@gmail.com>
2022-05-27 12:15:36 -0400

    kotlin introspection

--- kotlin-introspection/src/main/kotlin/DataClass.kt
+++ kotlin-introspection/src/main/kotlin/DataClass.kt
@@ -1,3 +1,4 @@
+import DataClassMeta.accessors
 import kotlinx.serialization.Serializable
 import kotlin.LazyThreadSafetyMode.PUBLICATION
 import kotlin.reflect.KMutableProperty1
@@ -5,16 +6,25 @@ import kotlin.reflect.KMutableProperty1
 @Serializable
 data class DataClass(var id: Long?, var data: String?, val readOnly: String?, var nonNull: Int = 0)
 
-val dataClassAccessors by lazy(PUBLICATION) {
-    mapOf(
-        "id" to DataClass::id,
-        "data" to DataClass::data,
-        "readOnly" to DataClass::readOnly,
-        "nonNull" to DataClass::nonNull,
-    )
+object DataClassMeta {
+    val accessors by lazy(PUBLICATION) {
+        mapOf(
+            "id" to DataClass::id,
+            "data" to DataClass::data,
+            "readOnly" to DataClass::readOnly,
+            "nonNull" to DataClass::nonNull,
+        )
+    }
 }
 
-fun DataClass.propMap() = dataClassAccessors
+@Serializable
+data class DbColumnMeta(
+    val columnName : String,
+    val columnSize: Int,
+    val decimalDigits: Int,
+    val datatype: String,
+    val isNullable: Boolean,
+)
 
 operator fun DataClass.iterator(): Iterator<Pair<String, Any?>> = listOf(
     "id" to id,
@@ -23,10 +33,10 @@ operator fun DataClass.iterator(): Iterator<Pair<String, Any?>> = listOf(
     "nonNull" to nonNull,
 ).iterator()
 
-operator fun DataClass.get(name: String): Any? = propMap()[name]?.get(this)
+operator fun DataClass.get(name: String): Any? = accessors[name]?.get(this)
 
 operator fun DataClass.set(name: String, value: Any?) {
-    val prop = propMap()[name]
+    val prop = accessors[name]
     if (prop is KMutableProperty1) {
         prop.set(this, value)
     } else throw IllegalArgumentException("No setter for $name")
