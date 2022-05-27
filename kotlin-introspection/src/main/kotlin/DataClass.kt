10562fd27cee2d8fe1a05b6444c2f8734ef74ef7
mgrouch <mgrushinskiy@gmail.com>
2022-05-27 11:26:33 -0400

    kotlin introspection

--- kotlin-introspection/src/main/kotlin/DataClass.kt
+++ kotlin-introspection/src/main/kotlin/DataClass.kt
@@ -2,24 +2,26 @@ import kotlinx.serialization.Serializable
 import kotlin.reflect.KMutableProperty1
 
 @Serializable
-data class DataClass(var id: Long?, var data: String?, val readOnly: String?)
+data class DataClass(var id: Long?, var data: String?, val readOnly: String?, var nonNull: Int = 0)
 
-val accessors = mapOf(
+val dataClassAccessors = mapOf(
     "id" to DataClass::id,
     "data" to DataClass::data,
     "readOnly" to DataClass::readOnly,
+    "nonNull" to DataClass::nonNull,
 )
 
 operator fun DataClass.iterator(): Iterator<Pair<String, Any?>> = listOf(
     "id" to id,
     "data" to data,
     "readOnly" to readOnly,
+    "nonNull" to nonNull,
 ).iterator()
 
-operator fun DataClass.get(name: String): Any? = accessors[name]?.get(this)
+operator fun DataClass.get(name: String): Any? = dataClassAccessors[name]?.get(this)
 
 operator fun DataClass.set(name: String, value: Any?) {
-    val prop = accessors[name]
+    val prop = dataClassAccessors[name]
     if (prop is KMutableProperty1) {
         prop.set(this, value)
     }
--- kotlin-introspection/src/main/kotlin/DataClassRunMe.kt
+++ kotlin-introspection/src/main/kotlin/DataClassRunMe.kt
@@ -1,10 +1,21 @@
 
 fun main(args: Array<String>) {
-    val obj = DataClass(1L, "name", "readOnly")
+    val obj = DataClass(1L, "name", "readOnly", -1)
     println("$obj")
-    println("${obj[DataClass::readOnly.name]}")
 
+    // it's kind of inefficient
+    println("${obj[DataClass::readOnly.name]} ${obj[DataClass::nonNull.name]}")
+
+    // better just
+    println("${DataClass::readOnly.get(obj)} ${DataClass::nonNull.get(obj)}")
+
+    // it's kind of inefficient
     obj[DataClass::data.name] = "name2"
+    obj[DataClass::nonNull.name] = -3
+    println("$obj")
+
+    // better just
+    DataClass::data.set(obj, "name3")
     println("$obj")
 
     for ((key, value) in obj) {
