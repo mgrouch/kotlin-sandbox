4c7f0418dd7e96117b0f63b5a7c498d94b9fd550
mgrouch <mgrushinskiy@gmail.com>
2022-06-06 21:27:21 -0400

    cleanup

--- kotlin-webgl/src/main/kotlin/com/example/Terrain.kt
+++ kotlin-webgl/src/main/kotlin/com/example/Terrain.kt
@@ -7,7 +7,6 @@ import info.laht.threekt.external.libs.Stats
 import info.laht.threekt.geometries.ConeGeometry
 import info.laht.threekt.geometries.PlaneGeometry
 import info.laht.threekt.materials.MeshBasicMaterial
-import info.laht.threekt.materials.MeshNormalMaterial
 import info.laht.threekt.math.Color
 import info.laht.threekt.math.Vector3
 import info.laht.threekt.objects.Mesh
@@ -17,10 +16,15 @@ import info.laht.threekt.scenes.Scene
 import info.laht.threekt.textures.Texture
 import io.kvision.html.Canvas
 import io.kvision.utils.px
+import io.kvision.utils.width
 import kotlinx.browser.document
 import kotlinx.browser.window
 import org.khronos.webgl.Uint8ClampedArray
+import org.khronos.webgl.get
+import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.Element
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.RenderingContext
 import kotlin.math.PI
 import kotlin.math.abs
 import kotlin.random.Random
@@ -71,7 +75,7 @@ class Terrain {
         controls.update()
 
         for (i in vertices.indices) {
-            vertices[i].z = (data[i] * 10).toDouble()
+            vertices[i].y = (data[i] * 10).toDouble()
         }
 
         val texture = Texture(generateTexture(data, worldWidth, worldDepth))
@@ -85,7 +89,7 @@ class Terrain {
         geometryHelper.translate(0, 50, 0)
         geometryHelper.rotateX(PI / 2)
 
-        val helper = Mesh(geometryHelper, MeshNormalMaterial())
+        val helper = Mesh(geometry, MeshBasicMaterial().apply{ color = Color(0xffff00) })
         scene.add(helper)
 
         val raycaster = camera.raycast()
@@ -121,20 +125,21 @@ class Terrain {
         val sun = Vector3(1, 1, 1)
         sun.normalize()
 
-        val canvas = Canvas()
-        // TODO: document.container.add(canvas)
-        val context = canvas.context2D
-        canvas.width = width.px
-        canvas.height = height.px
+        val canvas = document.createElement( "canvas" ) as HTMLCanvasElement
+        canvas.setAttribute("width", "$width")
+        canvas.setAttribute("height", "$height")
+
+        val context = canvas.getContext( "2d" ) as CanvasRenderingContext2D
         context.fillStyle = "#000"
         context.fillRect(0.0, 0.0, width.toDouble(), height.toDouble())
 
         val image = context.getImageData(
-            0.0, 0.0, canvas.width!!.first.toDouble(), canvas.height!!.first.toDouble()
+            0.0, 0.0, canvas.width.toDouble(), canvas.height.toDouble()
         )
         val imageData: Uint8ClampedArray = image.data
 
         var j = 0
+        val data1 = Array<Byte>(imageData.length) { 0 }
         for (i in 0 until imageData.length step 4) {
             vector3.x = data[j - 2].toDouble() - data[j + 2]
             vector3.y = 2.0
@@ -142,35 +147,36 @@ class Terrain {
             vector3.normalize()
 
             val shade = vector3.dot(sun)
-            // TODO: imageData[i] = ((96 + shade * 128) * (0.5 + data[j] * 0.007)).toUInt()
-            // TODO: imageData[i + 1] = ((32 + shade * 96) * (0.5 + data[j] * 0.007)).toUInt()
-            // TODO: imageData[i + 2] = ((shade * 96) * (0.5 + data[j] * 0.007)).toUInt()
+            data1[i] = ((96 + shade * 128) * (0.5 + data[j] * 0.007)).toInt().toByte()
+            data1[i + 1] = ((32 + shade * 96) * (0.5 + data[j] * 0.007)).toInt().toByte()
+            data1[i + 2] = ((shade * 96) * (0.5 + data[j] * 0.007)).toInt().toByte()
             j++
         }
+        image.data.set(data1, 0)
         context.putImageData(image, 0.0, 0.0)
 
         // Scaled 4x
-        val canvasScaled = Canvas()
-        // TODO: document.container.add(canvasScaled)
-        canvasScaled.width = (width * 4).px
-        canvasScaled.height = (height * 4).px
-
-        val canvasScaledContext = canvasScaled.context2D
+        val canvasScaled = document.createElement( "canvas" ) as HTMLCanvasElement
+        canvasScaled.setAttribute("width", "${width * 4}")
+        canvasScaled.setAttribute("height", "${height * 4}")
+        val canvasScaledContext = canvasScaled.getContext( "2d" ) as CanvasRenderingContext2D
         canvasScaledContext.scale(4.0, 4.0)
-        // TODO: canvasScaledContext.drawImage(image, 0.0, 0.0)
+        canvasScaledContext.drawImage( canvas, 0.0, 0.0 )
 
         val image2 = canvasScaledContext.getImageData(
-            0.0, 0.0, canvasScaled.width!!.first.toDouble(), canvasScaled.height!!.first.toDouble());
+            0.0, 0.0, canvasScaled.width.toDouble(), canvasScaled.height.toDouble())
         val imageData2 = image.data
 
+        val data2 = Array<Byte>(imageData.length) { 0 }
         for (i in 0 until imageData2.length step 4) {
             val v = (Random.nextDouble() * 5)
-            // TODO: imageData2[i] = imageData2[i].plus(v)
-            // TODO: imageData2[i + 1] = imageData2[i + 1].plus(v)
-            // TODO: imageData2[i + 2] = imageData2[i + 2].plus(v)
+            data2[i] = imageData2[i].plus(v).toInt().toByte()
+            data2[i + 1] = imageData2[i + 1].plus(v).toInt().toByte()
+            data2[i + 2] = imageData2[i + 2].plus(v).toInt().toByte()
         }
+        image2.data.set(data2, 0)
         canvasScaledContext.putImageData(image2, 0.0, 0.0)
-        return canvasScaled.getElementD()
+        return canvasScaled
     }
 
     fun animate() {
